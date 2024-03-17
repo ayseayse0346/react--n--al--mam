251f1caf95a54649acc95ee89a1f2ead915e4a8e
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-17 16:32:25 +0300

    add

---
+++ 2.component-yapısı/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Login from './Login'
+
+
+function App() {
+
+
+  return (
+    
+   <div>
+  <Login/>
+   </div>
+    
+  )
+}
+
+export default App
