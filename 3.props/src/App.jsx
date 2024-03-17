29fec6adddd65c30d1779891b15a9981ec54d2d7
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-17 18:49:13 +0300

    add

--- 3.props/src/App.jsx
+++ 3.props/src/App.jsx
@@ -21,7 +21,9 @@ function App() {
  <div>
    <Container>
     <hr/>
-    <Product/>
+    <Product productNme="pantalon" price={900}/>
+    <hr/>
+    <Product productNme="ayakkabı" price={900}/>
     </Container>
  </div>
 
