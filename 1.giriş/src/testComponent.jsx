234d14dcad36e0f449e2ee2d3e23baae237bfc6c
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-17 14:14:17 +0300

    rfce ile compenent hazır geliyor.kendimiz component açabiliyoruz

---
+++ 1.giriş/src/testComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function testComponent() {
+    // js kodları yazılır.
+  return (
+    //html kodları yazıloır.
+    <div>testComponent</div>
+  )
+}
+
+export default testComponent
